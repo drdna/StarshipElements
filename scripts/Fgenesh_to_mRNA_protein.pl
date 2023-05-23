e1be6b95ea3b3652afea09f7d3701bd48a101be4
Mark Farman <farman@email.uky.edu>
2023-05-23 11:47:06 -0400

    Update Fgenesh_to_mRNA_protein.pl

--- scripts/Fgenesh_to_mRNA_protein.pl
+++ scripts/Fgenesh_to_mRNA_protein.pl
@@ -1,19 +1,29 @@
 #!/usr/bin/perl
 
+$ARGV[0] =~ s/\W+$//;
+
+die "Input filename must end in _Starship_predicted_genes. Please rename file.\n" if $ARGV[0] !~ /_Starship_predicted_genes$/;
+
 open(FILE, $ARGV[0]);
 
-($ShortID = $ARGV[0]) =~ s/\..+//;
+($ShortID = $ARGV[0]) =~ s/_Starship_predicted_genes//;
 
-open(GENES, '>', "$ShortID"."_genes.fasta");
+print "$ShortID\n";
 
-open(PROTEINS, '>', "$ShortID"."_proteins.pep");
+($Path = $ARGV[0]) =~ s/(.+\/).+/$1/;
+
+$ShortID =~ s/.+\///;
+
+open(GENES, '>', "$Path"."$ShortID"."_genes.fasta") || die "Cannot open outfile $!\n";
+
+open(PROTEINS, '>', "$Path"."$ShortID"."_proteins.pep");
 
 while($Line = <FILE>) {
 
 	chomp($Line);
 	
 	if($Line =~ />/) {
-		
+
 		if($Line =~ /mRNA/) {
 
 			$Type = "Nucl"
@@ -25,8 +35,10 @@ while($Line = <FILE>) {
 			$Type = ""
 
 		}
-		
-		$Line =~ s/ +/_/g;
+
+        	$Line =~ s/FGENESH:/$ShortID/;
+
+        	$Line =~ s/ +/_/g;
 
 	}
 	
@@ -43,4 +55,3 @@ while($Line = <FILE>) {
 	}
 
 }
-
