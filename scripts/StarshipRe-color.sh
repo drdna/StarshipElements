241819d28c8fbfc67836116d49e0b41ec384eebf
Mark Farman <farman@email.uky.edu>
2023-12-09 09:15:41 -0500

    Update StarshipRe-color.sh

--- scripts/StarshipRe-color.sh
+++ scripts/StarshipRe-color.sh
@@ -2,10 +2,6 @@
 
 gff=$1
 
-#case $gff in (*recolored*)
-#echo $gff;;
-#esac
-
 if [[ ! "$gff" =~ recolored ]]; then
 awk -F '\t' '{if($3 ~ /5S/ && $5 - $4 > 100) {$9="color=#FF0000"substr($9, 14, 1000); print $0} else {print $0}; OFS="\t"}' $gff  | \
 grep -v color=#999999 > ${gff/\.gff/_recolored\.gff};
